673867d27b38715d36d98a8c75894235ebc86880
Your Name <youremail@example.com>
2026-06-07 20:57:55 +0530

    Added Java sum and product program

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello word"); 
+        int num1 = 4;
+        int num2 = 5;
+        int num3 = 6;
+        int sum = num1 + num2 + num3;
+        System.out.println("sum = " + sum);
+
+        int numn1 = 3;
+        int numn2 = 4;
+        int product = numn1 * numn2;
+        System.out.println("product = " + product);
+        
+        
+
+   
+    }
+}
+// add two number 
+// AddTwoNumber -->pascal naming convention
+// addTwoNumber -->camel case naming convention
+
+
+    
+
