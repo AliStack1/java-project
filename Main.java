d680ed420ee79d551111cfbab08b53879ab4188d
Your Name <youremail@example.com>
2026-06-10 10:40:33 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,20 +1,62 @@
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello word"); 
-        int num1 = 4;
-        int num2 = 5;
-        int num3 = 6;
-        int sum = num1 + num2 + num3;
-        System.out.println("sum = " + sum);
+       //System.out.println("my name is Ali");
+    Scanner sc = new Scanner(System.in);
+    //   System.out.println("enter no 1");
+  // int a = sc.nextInt();
+//System.out.println("enter no 2");
+//int b = sc.nextInt();
+//System.out.println("enter no 3");
+//int c = sc.nextInt();
+//int sum = a + b + c;
+//System.out.println("sum of three numbers: " + sum);
 
-        int numn1 = 3;
-        int numn2 = 4;
-        int product = numn1 * numn2;
-        System.out.println("product = " + product);
-        
+    //  boolean b1 = sc.hasNextInt();
+  //    System.out.println(b1);
+
+//  float f1 = sc.nextFloat();
+  // System.out.println(f1);
+
+      //   long ageDino = 20000000000L;
+       //   System.out.println(ageDino);
+
+         // String str = sc.nextLine();        // String str = sc.next();  // it will take only one word as input
+         // System.out.println(str);
+         System.out.println("Enter marks of subject 1");
+         int sub1 = sc.nextInt();
+            System.out.println("Enter marks of subject 2");
+            int sub2 = sc.nextInt();
+            System.out.println("Enter marks of subject 3");
+            int sub3 = sc.nextInt();
+            System.out.println("Enter marks of subject 4");
+            int sub4 = sc.nextInt();
+            System.out.println("Enter marks of subject 5");
+            int sub5 = sc.nextInt();
+
+            int totalMarks = sub1 + sub2 + sub3 + sub4 + sub5;
+            System.out.println("Total marks: " + totalMarks);
+            float percentage = (totalMarks / 500.0f) * 100;
+            System.out.println("percentage: " + percentage + "%");
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
         
 
-   
+
     }
 }
 // add two number 
